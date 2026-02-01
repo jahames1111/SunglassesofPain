cc1254eb9ee95b731dfb07cfe9c2cdd25a99a33b
jahames1111 <jfunky1111@gmail.com>
2026-02-01 09:51:16 -0500

    Add files via upload

---
+++ dumbsunglasses/keys.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+
+const char WIFI_SSID[] = "YOUR_SSID";
+const char WIFI_PASSWORD[] = "YOUR_PASS";
+
+
+const char GROQ_API_KEY[] = "YOUR_GROQ_API_KEY";
+const char WIT_TOKEN[]     = "YOUR_WIT_TOKEN";
